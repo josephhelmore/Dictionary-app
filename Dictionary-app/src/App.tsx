a5166396074500fd66fd486f26194ca6f9b5dd1c
admin <admin@admins-MacBook-Air.local>
2025-12-11 17:26:38 +0000

    initial commit

---
+++ Dictionary-app/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Header } from "./Components/Header";
+
+function App() {
+  return (
+    <section className="bg-blue-500" >
+      <Header />
+    </section>
+  );
+}
+
+export default App;
---
+++ Dictionary-app/src/Components/Header.tsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+    return <p className="header-text"> The Dictionary For Boomers</p>
+}
