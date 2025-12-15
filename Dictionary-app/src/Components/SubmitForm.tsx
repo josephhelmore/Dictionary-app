f90d271ba26ccab4a1f99c4d9ed7e4da59a6d1eb
admin <admin@admins-MacBook-Air.local>
2025-12-15 15:28:12 +0000

    add styling for different style screens

--- Dictionary-app/src/Components/SubmitForm.tsx
+++ Dictionary-app/src/Components/SubmitForm.tsx
@@ -27,7 +27,7 @@ export const SubmitForm = ({ setNewData }: SubmitFormProps) => {
     <>
       <form
         onSubmit={onSubmit}
-        className="flex flex-col text-center text-white font-mono justify-center bg-slate-600 m-8 rounded-lg py-8 shadow-xl/30"
+        className="flex flex-col text-center text-white font-mono justify-center bg-slate-600 m-8 rounded-lg py-8 shadow-xl/30 sm:flex-row"
       >
         <input
           value={newQuery}
@@ -38,27 +38,28 @@ export const SubmitForm = ({ setNewData }: SubmitFormProps) => {
           placeholder={placeholder}
           onFocus={() => setPlaceholder("")}
           id="text-area"
-          className={`bg-slate-400 border-2 rounded-xl h-25 mx-8 text-center 
+          className={`bg-slate-400 border-2 rounded-xl h-25 mx-8 text-center sm:w-75 
       ${isValid ? "border-slate-500" : "border-red-500"}`}
         />
-        <button
-          type="submit"
-          className="bg-slate-300 rounded-xl h-10 mx-8 mt-8 text-center text-black active:bg-slate-500"
-        >
-          Ask
-        </button>
-              <button
-        type="button"
-        onClick={async () => {
-          const data = await fetchRandom();
-          setNewData(data.data);
-        }}
-       className="bg-slate-300 rounded-xl h-10 mx-8 mt-8 text-center text-black active:bg-slate-500"
-      >
-        I'm Feeling Lucky
-      </button>
+        <div className="flex flex-col">
+          <button
+            type="submit"
+            className="bg-slate-300 rounded-xl h-10 mx-8 mt-8 text-center text-black active:bg-slate-500 sm:w-35 sm:m-0 sm:w-50"
+          >
+            Ask
+          </button>
+          <button
+            type="button"
+            onClick={async () => {
+              const data = await fetchRandom();
+              setNewData(data.data);
+            }}
+            className="bg-slate-300 rounded-xl h-10 mx-8 mt-8 text-center text-black active:bg-slate-500 sm:mx-0"
+          >
+            I'm Feeling Lucky
+          </button>
+        </div>
       </form>
-
     </>
   );
 };
