ce20b4eff1c20327969f198596e3e9fd210b3a23
admin <admin@admins-MacBook-Air.local>
2025-12-15 15:39:40 +0000

    update readme

--- Dictionary-app/src/Components/UserBody.tsx
+++ Dictionary-app/src/Components/UserBody.tsx
@@ -8,7 +8,7 @@ const [newData, setNewData] = useState<Record<string, any>[]>([]);
     <section className="h-auto bg-slate-700 pb-8">
       <p className="flex text-center text-white font-mono justify-center bg-slate-600 m-8 rounded-lg py-8 shadow-xl/30 ">
         There must be a word or phrase that you have always wanted to know the
-        meaning of... This is that time to find out.{" "}
+        meaning of... <br />This is that time to find out.{" "}
       </p>
       <SubmitForm setNewData={setNewData} />
       <OutputDefs newData={newData} />
